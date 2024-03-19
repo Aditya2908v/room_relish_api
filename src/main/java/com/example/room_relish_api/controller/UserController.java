cb49b6cf72aaa98e5092a5d0ba0b8e8cff188589
VETRIVEL Madhumitha - CIEC <madhumitha.vetrivel.ciec@skytv.it>
2024-03-19 17:12:03 +0530

    a

--- src/main/java/com/example/room_relish_api/controller/UserController.java
+++ src/main/java/com/example/room_relish_api/controller/UserController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/hello")
     public String hello(){
-        return "helloooooo";
+        return "hello from IDE";
     }
 }
