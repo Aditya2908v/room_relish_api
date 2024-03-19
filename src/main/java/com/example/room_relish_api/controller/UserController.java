7e2e33cc558ad67816e4da982c7316be5b62bd1d
Aditya Reddy <adityareddy1451@gmail.com>
2024-03-19 16:37:10 +0530

    sample end point

--- src/main/java/com/example/room_relish_api/controller/UserController.java
+++ src/main/java/com/example/room_relish_api/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.example.room_relish_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
 }
