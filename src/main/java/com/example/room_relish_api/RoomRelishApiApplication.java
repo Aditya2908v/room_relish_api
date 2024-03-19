eaecd6e473ad86fbfe09d26a5af75c9d65c8bef0
Aditya Reddy <adityareddy1451@gmail.com>
2024-03-19 15:55:20 +0530

    first commit

---
+++ src/main/java/com/example/room_relish_api/RoomRelishApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.room_relish_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRelishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRelishApiApplication.class, args);
+	}
+
+}
